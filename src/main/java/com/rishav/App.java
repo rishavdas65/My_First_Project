b5c7d55ea44b125e4a3d54a30c3c661ddd8881ba
rishav <rishavdas65@gmail.com>
2020-05-07 01:12:30 +0530

    .

---
+++ src/main/java/com/rishav/App.java
@@ -0,0 +1,13 @@
+package com.rishav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
